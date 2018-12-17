57baacf0d868f6b74772bcda98505b5d35afc599
jackielebwohl <43073290+jackielebwohl@users.noreply.github.com>
2018-12-17 13:15:41 -0500

    Add files via upload

---
+++ section05/01_insert_cats_into_cats_table.sql
@@ -0,0 +1,3 @@
+
+cursor.execute('''INSERT INTO cats (name, age, breed) VALUES ('Lil\ Bub', 5, 'American Shorthair');''')
+cursor.execute('''INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');''')
