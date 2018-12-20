73f5ccdfb8887ac4590d2e10296d0e68aa5bab5b
jackielebwohl <43073290+jackielebwohl@users.noreply.github.com>
2018-12-19 19:56:04 -0500

    Add files via upload

--- dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-pt-112618/shopping_cart.py
+++ dsc-1-07-07-object-oriented-shopping-cart-lab-online-ds-pt-112618/shopping_cart.py
@@ -1,50 +1,69 @@
+import numpy as np
 class ShoppingCart:
     def __init__(self, total=0, items =[], employee_discount=0):
         self._total=total
         self._items=items
         self._employee_discount=employee_discount 
-    def total (self):
-        return self._total
-    def items (self):
-        return self._items
-    def employee_discount(self):
-        return self._employee_discount
+    
+    #total
+    def get_total (self):
+        return f" Your total is ${self._total}."
+    def set_total (self, new_total):
+        self._total = self._total + new_total
+        return f" You have spent ${self._total} total." 
+        
+    total=property(get_total, set_total)
+        
+    
+    #items
+    def get_items (self):
+        return f" Your cart contains: {self._items}."
+    def set_items(self, list_of_items):
+        self._items = self._items.append(list_of_items)
+        return f"Your cart contains: {self._items}"
+    items=property(get_items, set_items)
+    
+    #employee discount
+    def get_employee_discount(self):
+        return f" Your employee discount is {self._employee_discount}% off."
+    def set_employee_discount(self, discount=0):
+        self._employee_discount=discount
+        return f" You're next order will receive {self._employee_discount}% off."
+    employee_discount=property(get_employee_discount, set_employee_discount)
     
     def add_item(self, item, price, quantity=1):
         item_dict={'item': item, 'price': price, 'quantity': quantity}
         self._items.append(item_dict)
         self._total += price * quantity
-        return self._total
+        return f" You are purchasing {self._items} for ${self._total}"
    
     def mean_item_price(self):
         num_items=[]
         for item in self._items:
             num_items.append(item['quantity'])
-        return self._total / sum(num_items)
- #   def median_item_price(self):
-  #      list_of_prices=[]
-   #     for item in self._items:
-    #        if item['quantity'] == 1:
-     #           list_of_prices.append(item['price'])
-      #      else:
-       #         list_of_prices.append([item['price']] * item['quantity'])    
-        #return list_of_prices
-        #prices_sorted=float(list_of_prices).sort()
-        #if len(list_of_prices) %2 == 0:
-         #   index_1= int(len(list_of_prices))/2 - 1
-          #  index_2= int(len(list_of_prices))/2 
-           # median= (prices_sorted[index_1] + prices_sorted[index_2]) / 2
-            #return median
-       # else:
-        #    index_number= (len(list_of_prices)-1) / 2
-         #   return prices_sorted[index_number]
+        mean = self._total / sum(num_items)
+        return f"Your average item costs ${mean}."
+    
+    def median_item_price(self):
+        list_of_prices=[]
+        for item in self._items:
+            iteration_count = item['quantity']
+            while iteration_count >0:
+                list_of_prices.append(item['price'])
+                iteration_count -= 1
+            else: 
+                continue
+        median = np.median(list_of_prices)
+        return f"Your median item price is {median}."
            
     def apply_discount(self):
         if self._employee_discount:
             multiplier = (100-self._employee_discount)/100
-            return self._total * multiplier
+            discounted_total = self._total * multiplier
+            return f" Your discounted total at {self._employee_discount}% off is ${discounted_total}."
         else:
             return "Sorry, there is no discount to apply to your cart :("
+    
     def item_names (self):
         list_of_items=[]
         for item in self._items:
@@ -54,7 +73,8 @@ class ShoppingCart:
                 iteration_count -= 1
             else:
                 continue
-        return list_of_items
+        return f" You are purchasing {list_of_items}."
+   
     def void_last_item(self):
         if self._items:
             removed_item=self._items.pop()
